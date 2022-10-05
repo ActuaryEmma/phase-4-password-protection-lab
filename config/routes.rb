90291d8cf3e7f4f53622bb730b8e62a6d82d7af1
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-10-05 16:04:09 +0300

    commit tree bceb394b2bc33d9b3570698fa3a641dc1b7cf19f

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+
+  post "/signup", to: "users#create"
+  # post "/me", to: "users#show"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
